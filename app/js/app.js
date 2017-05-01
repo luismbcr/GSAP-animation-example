596a05b3e79b01565d3fb6435b69627c23f6354c
Luis Montero <luismb@luismb-thi-m.local>
2017-04-30 21:40:54 -0600

    Using delay option

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 function startTween(){
     // params : ELEMENT, TIME in SECONDS, Literal Object with custom animation
-    TweenLite.to($('#toolBox'),1,{x:290, y:230, scale:1.5, onComplete:returnToNormal});
+    TweenLite.to($('#toolBox'),1,{delay:1,x:290, y:230, scale:1.5, onComplete:returnToNormal});
     TweenLite.to($('.asapReg'),1, {color: "#ff0000", fontSize: "+=75"})
 }
 
