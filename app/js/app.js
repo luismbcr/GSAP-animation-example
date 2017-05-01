9d38772ee02294bf18f71ba2c1bda8a84414d25a
Luis Montero <luismb@luismb-thi-m.local>
2017-05-01 13:20:09 -0600

    Timeline params to pause, and use ease animations

--- app/js/app.js
+++ app/js/app.js
@@ -6,12 +6,13 @@ $(document).ready(function(){
 })
 
 function init(){
-    var toolTimeline = new TimelineLite();
-    var duration = 1;
-    toolTimeline.add( TweenLite.to($('#toolBox'), duration,{y: -100}));
-    toolTimeline.add( TweenLite.to($('#weightLifter'), duration,{y: -100}));
-    toolTimeline.add( TweenLite.to($('#crazy'), duration,{y: -100}));
-    toolTimeline.add( TweenLite.to($('#leaf'), duration,{y: -100}));
+    //Pass params to timeline
+    var toolTimeline = new TimelineLite({paused: true});
+    var duration = 0.5;
+    toolTimeline.add( TweenLite.to($('#toolBox'), duration,{y: -100, ease: Linear.easeInOut}));
+    toolTimeline.add( TweenLite.to($('#weightLifter'), duration,{y: -100, ease: Bounce.easeInOut}));
+    toolTimeline.add( TweenLite.to($('#crazy'), duration,{y: -100, ease: Elastic.easeInOut}));
+    toolTimeline.add( TweenLite.to($('#leaf'), duration,{y: -100, ease: Back.easeInOut}));
 
     //Events
     //Jquery elements
