ad2b072b736c8f3491eb17efa82362ca292ad28d
Luis Montero <luismb@luismb-thi-m.local>
2017-04-30 21:38:18 -0600

    Reset to normal

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 
 function startTween(){
     // params : ELEMENT, TIME in SECONDS, Literal Object with custom animation
-    TweenLite.to($('#toolBox'),1,{x:290, y:230, scale:1.5});
+    TweenLite.to($('#toolBox'),1,{x:290, y:230, scale:1.5, onComplete:returnToNormal});
     TweenLite.to($('.asapReg'),1, {color: "#ff0000", fontSize: "+=75"})
+}
+
+function returnToNormal(){
+    TweenLite.to($('#toolBox'),1,{x:0, y:0, scale:1});
 }
